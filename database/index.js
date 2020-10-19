441b26505ded52ffd4df8608c4e537be91c2e0c1
gitHandle <earturoga@gmail.com>
2020-10-19 16:33:29 -0600

    added database data formatting

--- database/index.js
+++ database/index.js
@@ -1,70 +1,163 @@
-/**DATABASE CONNECTION**/
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/stubbs', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017";
+//connection url
+const dbName = 'stubbs'
+let db;
+let mongoClient;
+
+MongoClient.connect(url, {useNewUrlParser:true, useUnifiedTopology:true},
+  ( err, client ) => {
+  if (err) {
+    console.log('Connection error to mongodb:', err);
+  } else {
+    console.log('Connected to mongo database');
+    mongoClient = client;
+    db = client.db(dbName);
+  }
 });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected to Mongo Database!');
-});
+const findProduct = (productId)=> {
+  return new Promise((resolve, reject) => {
+    db.collection('product').findOne({'id': productId}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  });
+}
 
-//a schema will define how our collection will look like
-//since schema is already defined we can include an empty one
-const productSchema = new mongoose.Schema({});
-const Product = mongoose.model('Product', productSchema, 'product');
+const findStyles = (productId)=> {
+  return new Promise((resolve, reject) => {
+    db.collection('styles').findOne({'productId': productId}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  });
+}
+const findSkus = (productId)=> {
+  return new Promise((resolve, reject) => {
+    db.collection('skus').findOne({'productId': productId}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  });
+}
+const findPhotos = (productId)=> {
+  return new Promise((resolve, reject) => {
+    db.collection('photos').findOne({'productId': productId}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  });
+}
+const findFeatures = (productId)=> {
+  return new Promise((resolve, reject) => {
+    db.collection('features').findOne({'productId': productId}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  });
+}
 
-const findOneProduct = (productId, callback) => {
-  Product.findOne({"id": productId}, (err, res) => {
-    console.log(err, res);
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, res);
+const formatAllData = (dataArray) => {
+  let product = dataArray[0];
+  //if no product returned then it does not exist;
+  if (!product) {
+    return undefined;
+  }
+  let style = dataArray[1];
+  let skus = dataArray[2];
+  let photos = dataArray[3];
+  let features = dataArray[4];
+  if (!style) {
+    style = {};
+    style.stylesIds = [];
+    style.totalStyles = 0;
+    style.styles = {};
+  } 
+  if (!skus) {
+    skus = {};
+    skus.size = {};
+    skus.totalSizes = 0;
+  }
+  if (!photos) {
+    photos = {};
+    photos.photos = {};
+  }
+  if (!features) {
+    features = {};
+    features.features = {};
+    features.totalFeatures = 0;
+  }
+  let completeProductData = {
+  productId: product.id,
+  name: product.name,
+  slogan: product.slogan,
+  description: product.description,
+  category: product.category,
+  default_price: product.default_price,
+  stylesIds: style.stylesIds,
+  totalStyles: style.totalStyles,
+  styles: style.styles,
+  size: skus.size,
+  totalSizes: skus.totalSizes,
+  photos: photos.photos,
+  features: features.features,
+  totalFeatures: features.totalFeatures,
+  schema_version: 1,
+}
+  return completeProductData;
+}
+
+const insertCompleteProduct = (data) => {
+  return new Promise((resolve, reject) => {
+    db.collection('productAll').updateOne({productId: data.productId}, {$set: data}, {upsert: true})
+    .then((res) => {
+      console.log('Inserted or Modified:', res.result );
+    })
+    .catch((err) => {
+      console.log('Error inserting:', err );
+    })
+  });
+}
+
+const getProductData = (productId, callback) => {
+  let id = parseInt(productId);
+  let product = findProduct(id);
+  let style = findStyles(id);
+  let photos = findPhotos(id);
+  let skus = findSkus(id);
+  let features = findFeatures(id);
+  Promise.all([product, style, skus, photos, features])
+  .then((results) => {
+    if (!results[0]) {
+      throw new Error('No Product found');
     }
+    //we insert into our database
+    let formatted = formatAllData(results);
+    insertCompleteProduct(formatted);
+    callback(null, formatted);
+  })
+  .catch((err) => {
+    // console.log('Error:', err);
+    callback(err, null);
   })
 }
-// findOneProduct(1000000, (err, res) => {
-//   console.log(res, 'find');
-// })
-const styleSchema = new mongoose.Schema({});
-const Style = mongoose.model('Style', styleSchema, 'styles');
-
-const findStyles = (productId, callback) => {
-  Style.find({"productId": productId}, (err, res) => {
-    if (err) {
-      console.log('error:', err);
-    } else {
-      res.forEach((style) => {
-        console.log(style);
-      })
-    }
-  })
-}
-// findStyles(1000);
-const skusSchema = new mongoose.Schema({});
-const Skus = mongoose.model('Skus', skusSchema, 'skus');
-
-const findSkus = (productId, callback) => {
-  Skus.find({"id": productId}, (err, res) => {
-    if (err) {
-      // callback(err, null);
-      console.log(err);
-    } else {
-      console.log(res);
-      // callback(null, res);
-    }
-  })
-}
-findSkus(1000);
-
-
 
 module.exports = {
-  findOneProduct: findOneProduct,
-  findStyles: findStyles,
+  getProductData,
 };
