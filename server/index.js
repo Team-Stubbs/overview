48e9e154c37d23618d24cbb16feb3cc75e4f2d5a
gitHandle <earturoga@gmail.com>
2020-10-19 16:32:43 -0600

    added server handler for product data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+const dbMethods = require('../database/index.js');
 
 app.use(express.json());
+let CACHE = {};
+let cacheCount = 0;
 
+app.get('/:productId', (req, res) => {
+  let id = req.params.productId;
+  if (CACHE[id]) {
+    res.send(CACHE[id]);
+    console.log('In last cache');
+    return;
+  }
+  dbMethods.getProductData(id, (err, result) => {
+    if (err) {
+      res.status(500).send('Product Not Found');
+    } else {
+      //allow up to 3 Caches
+      cacheCount++;
+      if (cacheCount > 3) {
+        CACHE = {};
+        cacheCount = 0;
+      }
+      CACHE[id] = result;
+      res.send(result);
+    }
+  })
+});
 
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening at http://localhost:${PORT}`);
 });
