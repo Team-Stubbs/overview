19137df53604a1348b89730e92b1b4ea0284e504
gitHandle <earturoga@gmail.com>
2020-10-21 15:28:10 -0600

    fixed last commit errors in database index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const LRU = require('lru-cache');
 const cache = new LRU({max: 250})
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = 4000;
 const dbMethods = require('../database/index.js');
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/:productId', (req, res) => {
+app.get('/product/:productId', (req, res) => {
   let id = req.params.productId;
   let inCache = cache.get(id)
   if (inCache) {
